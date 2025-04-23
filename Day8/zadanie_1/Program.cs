f14076dc899f81430d73fd37fc32f1dfec7a06b6
ksundel228 <urcikksenia2007@gmail.com>
2025-04-23 09:32:47 +0300

    Add Task to Day 8

---
+++ Day8/zadanie_1/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace _1
+{
+    class Program
+    {
+        struct ZNAK
+        {
+            public string name, surname;
+            public string zodiacSign;
+            public int[] birthday;
+        }
+        static void Main(string[] args)
+        {
+            ZNAK[] mass = new ZNAK[8];
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine("Введите имя:");
+                mass[i].name = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine("Введите фамилию:");
+                mass[i].surname = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine("Введите знак Зодиака:");
+                mass[i].zodiacSign = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine("Введите дату рождения (день, месяц, год):");
+                mass[i].birthday = new int[3];
+                for (int j = 0; j < mass[i].birthday.Length; j++)
+                {
+                    string input = Console.ReadLine() ?? "0";
+                    if (!int.TryParse(input, out mass[i].birthday[j]))
+                    {
+                        Console.WriteLine("Ошибка: введите корректное число!");
+                        j--;
+                    }
+                }
+            }
+            Sort(mass);
+
+            Console.WriteLine("Введите знак Зодиака для поиска:");
+            string searchZodiacSign = Console.ReadLine() ?? string.Empty;
+            bool found = false;
+
+            foreach (var person in mass)
+            {
+                if (person.zodiacSign.Equals(searchZodiacSign, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Имя: " + person.name);
+                    Console.WriteLine("Фамилия: " + person.surname);
+                    Console.WriteLine("Знак Зодиака: " + person.zodiacSign);
+                    Console.WriteLine("Дата рождения: " + string.Join(".", person.birthday));
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Человек с таким знаком Зодиака не найден.");
+            }
+        }
+
+        static bool isMin(ZNAK first, ZNAK second)
+        {
+            if (first.birthday[2] < second.birthday[2]) return false;
+            else if (first.birthday[2] > second.birthday[2]) return true;
+            else if (first.birthday[1] < second.birthday[1]) return false;
+            else if (first.birthday[1] > second.birthday[1]) return true;
+            else if (first.birthday[0] < second.birthday[0]) return false;
+            else if (first.birthday[0] > second.birthday[0]) return true;
+            else return false;
+        }
+
+        static void Sort(ZNAK[] mass)
+        {
+            ZNAK buf;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                for (int j = i + 1; j < mass.Length; j++)
+                {
+                    if (isMin(mass[i], mass[j]))
+                    {
+                        buf = mass[i];
+                        mass[i] = mass[j];
+                        mass[j] = buf;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Day8/zadanie_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EmployeeBonus
+{
+    enum Post
+    {
+        Manager = 160,
+        Developer = 150,
+        Designer = 140,
+        Tester = 130     
+    }
+
+    class Accauntant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            return hours > (int)worker;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accauntant accountant = new Accauntant();
+
+            Post[] workers = { Post.Manager, Post.Developer, Post.Designer, Post.Tester };
+            int[] workedHours = { 170, 145, 150, 135 };
+
+            for (int i = 0; i < workers.Length; i++)
+            {
+                Console.WriteLine($"Проверка для сотрудника: {workers[i]}");
+
+                if (accountant.AskForBonus(workers[i], workedHours[i]))
+                {
+                    Console.WriteLine("Сотруднику положена премия!");
+                }
+                else
+                {
+                    Console.WriteLine("Сотруднику не положена премия.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
