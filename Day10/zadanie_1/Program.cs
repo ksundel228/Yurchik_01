94b0d29a8f1abe10e0c31e7047bd406ca0328323
ksundel228 <urcikksenia2007@gmail.com>
2025-04-25 22:17:49 +0300

    Add Task to Day 9

---
+++ Day10/zadanie_1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Zadanie1
+{
+    // Класс Apartment (Квартира)
+    class Apartment
+    {
+        private string name;       // Название квартиры
+        private double costPerM2; // Стоимость 1 м²
+        private double area;      // Площадь
+
+        // Конструктор с параметрами
+        public Apartment(string name, double costPerM2, double area)
+        {
+            this.name = name;
+            this.costPerM2 = costPerM2;
+            this.area = area;
+        }
+
+        // Метод для расчета стоимости квартиры
+        public virtual double CalculateCost()
+        {
+            return costPerM2 * area;
+        }
+
+        // Методы доступа
+        public string Name { get { return name; } set { name = value; } }
+        public double CostPerM2 { get { return costPerM2; } set { costPerM2 = value; } }
+        public double Area { get { return area; } set { area = value; } }
+
+        // Метод для вывода информации о квартире
+        public void GetInfo()
+        {
+            Console.WriteLine("Название: {0} | Стоимость 1м²: {1} | Площадь: {2}", name, costPerM2, area);
+        }
+    }
+
+    // Класс CenterApartment (Квартира в центре), наследник Apartment
+    class CenterApartment : Apartment
+    {
+        private string district; // Название района
+
+        // Конструктор с параметрами
+        public CenterApartment(string name, double costPerM2, double area, string district)
+            : base(name, costPerM2, area)
+        {
+            this.district = district;
+        }
+
+        // Переопределение метода для расчета стоимости
+        public override double CalculateCost()
+        {
+            double baseCost = base.CalculateCost();
+            return baseCost + (baseCost * 0.01); // Надбавка за расположение
+        }
+
+        // Метод для вывода информации о квартире в центре
+        public new void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("Район: {0} | Общая стоимость (с надбавкой): {1}", district, CalculateCost());
+        }
+    }
+
+    // Основной класс Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта базового типа
+            Apartment apartment = new Apartment("Обычная квартира", 1000, 50);
+            apartment.GetInfo();
+            Console.WriteLine("Стоимость квартиры: {0}\n", apartment.CalculateCost());
+
+            // Создание объекта производного типа
+            CenterApartment centerApartment = new CenterApartment("Квартира в центре", 1500, 60, "Центральный район");
+            centerApartment.GetInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day10/zadanie_2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Zadanie2
+{
+    // Класс A
+    class A
+    {
+        private int a; // Поле a
+        private int b; // Поле b
+
+        // Свойство c: сумма полей a и b
+        public int C
+        {
+            get { return a + b; }
+        }
+
+        // Конструктор по умолчанию
+        public A()
+        {
+            a = 5; // Инициализация поля a
+            b = 10; // Инициализация поля b
+        }
+    }
+
+    // Класс B (наследник A)
+    class B : A
+    {
+        private int d; // Поле d
+
+        // Свойство c2: произведение полей a, b, и d с использованием оператора while
+        public int C2
+        {
+            get
+            {
+                int result = 1;
+                int counter = 0;
+
+                while (counter < d)
+                {
+                    result *= C; // C - свойство из родительского класса
+                    counter++;
+                }
+
+                return result;
+            }
+        }
+
+        // Конструктор, наследующийся от класса A
+        public B() : base()
+        {
+            d = 3; // Инициализация поля d
+        }
+
+        // Собственный конструктор
+        public B(int dValue) : base()
+        {
+            d = dValue;
+        }
+    }
+
+    // Основной класс Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта класса A
+            A objA = new A();
+            Console.WriteLine("Свойство c (класс A): {0}\n", objA.C);
+
+            // Создание объекта класса B с использованием наследуемого конструктора
+            B objB1 = new B();
+            Console.WriteLine("Свойство c2 (класс B, конструктор 1): {0}\n", objB1.C2);
+
+            // Создание объекта класса B с использованием собственного конструктора
+            B objB2 = new B(5);
+            Console.WriteLine("Свойство c2 (класс B, конструктор 2): {0}\n", objB2.C2);
+
+            Console.ReadLine();
+        }
+    }
+}
