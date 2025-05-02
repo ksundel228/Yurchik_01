ccbb6a7b930a76e5be3ae68c37ebc9d94e227fe0
ksundel228 <urcikksenia2007@gmail.com>
2025-05-02 10:58:19 +0300

    доп файлы

--- PrilozhenieA/zadanie_4/obj/Debug/net9.0/zadanie_4.AssemblyInfo.cs
+++ PrilozhenieA/zadanie_4/obj/Debug/net9.0/zadanie_4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zadanie_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24f191a0f8e58f680fc324c9913775f3ec927f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a246c0577ea3775b926747eeef170e8843ac69")]
 [assembly: System.Reflection.AssemblyProductAttribute("zadanie_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zadanie_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
