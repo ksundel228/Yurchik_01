8a8ade750f13675653115d92074eedccaec06905
ksundel228 <urcikksenia2007@gmail.com>
2025-05-21 11:42:09 +0300

    день 25

---
+++ Day25/zadanie_1/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,            //// Где находятся тематические словари ресурсов
+                                                //(используются, если ресурс не найден на странице,
+                                                // или в словарях ресурсов приложения)
+    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
+                                                //((используются, если ресурс не найден на странице,
+                                                // приложении или в любых тематических словарях ресурсов)
+)]
+
